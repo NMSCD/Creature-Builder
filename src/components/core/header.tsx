e67c3a4a5eb12dec787cc5b8f3d8e38b3ba00a7c
Kurt Lourens <kurt.lourens@outlook.com>
2022-05-23 00:32:26 +0200

    rel path and build working

--- src/components/core/header.tsx
+++ src/components/core/header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header: React.FC = () => {
     return (
         <header>
-            <img src="/assets/img/banner.svg" alt="banner" style={{ margin: '0 auto' }} />
+            <img src="./assets/img/banner.svg" alt="banner" style={{ margin: '0 auto' }} />
         </header>
     );
 }
