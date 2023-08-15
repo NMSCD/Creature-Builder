bdeca0dc920f7b9ba2fb26e3f466d4c6666d84ad
Kurt Lourens <hi@kurtlourens.com>
2023-08-15 21:04:12 +0200

    📱 Better responsiveness

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 export const Header: React.FC = () => {
     return (
         <header className="noselect" draggable="false">
-            <img src="./assets/img/banner.svg" alt="banner" draggable="false" style={{ margin: '0 auto' }} />
+            <picture draggable="false" >
+                <source srcSet="./assets/img/banner.svg" media="(min-width: 650px)" />
+                <img src="./assets/img/banner-small.png" alt="banner" draggable="false" style={{ margin: '0 auto', padding: '0 3em' }} />
+            </picture>
         </header>
     );
 }
